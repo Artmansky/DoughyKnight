afa5bd8183179f6b67d400195ef2ca29354ed997
Artmansky <tomaszartmanski@gmail.com>
2024-05-08 22:09:02 +0200

    Initial commit

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D rigid;
+
+    Vector2 movement;
+
+    void Update()
+    {
+        MovementInput();
+    }
+
+    void FixedUpdate()
+    {
+        rigid.velocity = movement * moveSpeed;
+    }
+
+    void MovementInput()
+    {
+        float speedX = Input.GetAxisRaw("Horizontal");
+        float speedY = Input.GetAxisRaw("Vertical");
+
+        movement = new Vector2(speedX, speedY).normalized;
+    }
+}
