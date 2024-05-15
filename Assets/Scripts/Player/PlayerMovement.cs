9ff092e9da40a13a252e1f98522c03ec943fde62
Artmansky <tomaszartmanski@gmail.com>
2024-05-15 22:39:38 +0200

    Proper character animations

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,12 +6,15 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
+    public Animator animator;
 
     private Rigidbody2D rigid;
     private Vector2 movement;
+    private bool isFacingRight = true;
     void Start()
     {
         rigid = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
@@ -21,6 +25,7 @@ public class PlayerMovement : MonoBehaviour
     void FixedUpdate()
     {
         rigid.velocity = movement * moveSpeed;
+        animator.SetFloat("Velocity", Math.Abs(rigid.velocity.x));
     }
 
     void MovementInput()
@@ -28,6 +33,19 @@ public class PlayerMovement : MonoBehaviour
         float speedX = Input.GetAxisRaw("Horizontal");
         float speedY = Input.GetAxisRaw("Vertical");
 
+        FlipSprite(speedX);
+
         movement = new Vector2(speedX, speedY).normalized;
+    }
+
+    void FlipSprite(float horizontal)
+    {
+        if(isFacingRight && horizontal<0f || !isFacingRight && horizontal > 0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 ls = transform.localScale;
+            ls.x *= -1f;
+            transform.localScale = ls;
+        }
     }
 }
